38a4caaeccc23fc729d857db7f9a4fc6e05a2a9a
Petter Daae <petter.daae@gmail.com>
2018-09-14 09:01:35 +0200

    added names, formatting and readme.md

--- main.py
+++ main.py
@@ -1,92 +1,70 @@
 import requests
 import xlsxwriter
-from bs4 import BeautifulSoup
-from datastructures import *
-
-
-def findNoPages(baseUrl=None, headers=None):
-    assert baseUrl is not None
-    assert headers is not None
-    response = requests.get(baseUrl, headers=headers)
-    soup = BeautifulSoup(response.text, features="html.parser")
-
-    lis = soup.find_all("li", {"class": "page-item"})
-    return int(lis[3].text)
-
-
-
-def getStudent(id=None, students=None):
-    assert id is not None
-    assert students is not None
-
-    for student in students:
-        if student.id == id:
-            return student
-    
-    return None
-
-
-def getAssignmentsFromOnePage(studentsList=None, baseUrl=None, pageNumber=None, headers=None):
-    assert studentsList is not None
-    assert pageNumber is not None
-    assert baseUrl is not None
-    assert headers is not None
-
-    response = requests.get(baseUrl + "&page=" + str(pageNumber), headers=headers)
-    soup = BeautifulSoup(response.text, features="html.parser")
-    
-    trs = soup.find_all("tr")
-
-    for i in range(1, len(trs)):
-        tds = trs[i].find_all("td")
-        id = tds[0].text
-        assignment = tds[1].text
-        group = tds[2].text
-        score = tds[3].text
-        max = tds[4].text
-
-        assignment = Assignment(group=group, assignment=assignment, score=score, max=max)
-
-        student = getStudent(id=id, students=studentsList)
-
-        if student is None:
-            newStudent = Student(id=id)
-            newStudent.addAssignment(assignment)
-            studentsList.append(newStudent)
-        else:
-            student.addAssignment(assignment)
-
-cookie = ""
+from utils import *
+import time
 
 baseUrl = "https://puffin.ii.uib.no/submissions/?category=&group=&assignment=&filtering=my-student"
-headers = { 
-    "Cookie": cookie
-}
-
-noPages = findNoPages(baseUrl=baseUrl, headers=headers)
+profileUrl = "https://puffin.ii.uib.no/account/profile/"
+cookie = ""
+headers = { "Cookie": cookie }
 
 students = list()
+getStudents(url=profileUrl, headers=headers, studentsList=students)
+noPages = findNoPages(baseUrl=baseUrl, headers=headers)
 
 for i in range(noPages):
     getAssignmentsFromOnePage(studentsList=students, baseUrl=baseUrl, pageNumber=(i + 1), headers=headers)
 
-
-
-
-
-
-
-workbook = xlsxwriter.Workbook('new.xlsx')
+workbook = xlsxwriter.Workbook(str(time.time()) + '.xlsx')
 worksheet = workbook.add_worksheet()
 
+currentAssignments = ["Assignment 0","Assignment 1","Assignment 2","Assignment 3"]
+dataArea = "C2:" + getLetterIndex(len(currentAssignments) + 2) + str(len(students) + 1)
+
+worksheet.write(0, 0, len(students))
+
+for i in range(len(currentAssignments)):
+    worksheet.write(0, i + 2, currentAssignments[i])
 
 for i in range(len(students)):
-    
-    worksheet.write(i, 0, students[i].id)
+    worksheet.write(i + 1, 0, students[i].id)
+    worksheet.write(i + 1, 1, students[i].name)
     weekly = students[i].getWeeklyExercises()
 
-    for j in range(len(weekly)):
-        a = weekly[j]
-        worksheet.write(i, j + 1, (a))
+    for j in range(len(currentAssignments)):
+        assignment = currentAssignments[j]
+        studentScore = students[i].getPercentScore(assignment)
+        if studentScore is not None:
+            worksheet.write(i + 1, j + 2, studentScore)
+
+# Light red fill with dark red text.
+format1 = workbook.add_format({'bg_color':   '#FFC7CE',
+                               'font_color': '#9C0006'})
+
+# Light yellow fill with dark yellow text.
+format2 = workbook.add_format({'bg_color':   '#FFEB9C',
+                               'font_color': '#9C6500'})
+
+# Green fill with dark green text.
+format3 = workbook.add_format({'bg_color':   '#C6EFCE',
+                               'font_color': '#006100'})
+
+
+worksheet.conditional_format(dataArea, {'type': 'cell',
+                                       'criteria': 'between',
+                                       'minimum': 0.01,
+                                       'maximum': 0.99,
+                                       'format': format2})
+
+worksheet.conditional_format(dataArea, {'type': 'cell',
+                                       'criteria': 'equal to',
+                                       'value': 1,
+                                       'format': format3})
+
+worksheet.conditional_format(dataArea, {'type': 'cell',
+                                       'criteria': 'equal to',
+                                       'value': 0,
+                                       'format': format1})
+
 
 workbook.close()
---
+++ utils.py
@@ -0,0 +1,138 @@
+import requests
+from bs4 import BeautifulSoup
+
+class Student:
+
+    def __init__(self, id=None, name="No Name"):
+        assert id is not None
+        self.id = id
+        self.name = name
+        self.assignments = list()
+
+    def addAssignment(self, assignment):
+        self.assignments.append(assignment)
+
+    def getWeeklyExercises(self):
+        we = list()
+        for a in self.assignments:
+            if a.group == "Weekly":
+                we.append(a.assignment)
+        we.sort()
+        return we
+
+    def getAssignment(self, name=None):
+        assert name is not None
+        for a in self.assignments:
+            if a.assignment == name:
+                return a
+        return None
+
+    def getPercentScore(self, name=None):
+        assert name is not None
+        assignment = self.getAssignment(name=name)
+        if assignment is None:
+            return 0
+        if float(assignment.max) == 0:
+            return 1
+        return (float(assignment.score) / float(assignment.max))
+
+class Assignment:
+
+    def __init__(self, group=None, assignment=None, score=None, max=None):
+        assert group is not None
+        assert assignment is not None
+        assert score is not None
+        assert max is not None
+
+        self.group = group
+        self.assignment = assignment
+        self.score = score
+        self.max = max
+
+    def __str__(self):
+        return (self.group + ": " + self.assignment)
+
+
+def getStudent(id=None, students=None):
+    assert id is not None
+    assert students is not None
+
+    for student in students:
+        if student.id == id:
+            return student
+    
+    return None
+
+def findNoPages(baseUrl=None, headers=None):
+    assert baseUrl is not None
+    assert headers is not None
+    response = requests.get(baseUrl, headers=headers)
+    soup = BeautifulSoup(response.text, features="html.parser")
+
+    lis = soup.find_all("li", {"class": "page-item"})
+    return int(lis[3].text)
+
+def getAssignmentsFromOnePage(studentsList=None, baseUrl=None, pageNumber=None, headers=None):
+    assert studentsList is not None
+    assert pageNumber is not None
+    assert baseUrl is not None
+    assert headers is not None
+
+    response = requests.get(baseUrl + "&page=" + str(pageNumber), headers=headers)
+    soup = BeautifulSoup(response.text, features="html.parser")
+    
+    trs = soup.find_all("tr")
+
+    for i in range(1, len(trs)):
+        tds = trs[i].find_all("td")
+        id = tds[0].text
+        assignment = tds[1].text
+        group = tds[2].text
+        score = tds[3].text
+        max = tds[4].text
+
+        assignment = Assignment(group=group, assignment=assignment, score=score, max=max)
+
+        student = getStudent(id=id, students=studentsList)
+
+        if student is None:
+            newStudent = Student(id=id)
+            newStudent.addAssignment(assignment)
+            studentsList.append(newStudent)
+        else:
+            student.addAssignment(assignment)
+
+def getStudents(url=None, headers=None, studentsList=None):
+    assert url is not None
+    assert studentsList is not None
+    assert headers is not None
+
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, features="html.parser")
+    lis = soup.find_all("li")
+    noPages = int(lis[len(lis) - 2].text)
+
+    for i in range(noPages):
+        getStudentsFromPage(baseUrl=url, headers=headers, studentsList=studentsList, pageNumber=(i + 1))
+
+def getStudentsFromPage(baseUrl=None, headers=None, studentsList=None, pageNumber=None):
+    assert baseUrl is not None
+    assert headers is not None
+    assert studentsList is not None
+    assert pageNumber is not None
+
+    response = requests.get(baseUrl + "?student_page=" + str(pageNumber), headers=headers)
+    soup = BeautifulSoup(response.text, features="html.parser")
+
+    ls = soup.find("div", id="recommended_list")
+    a = ls.find_all("a", {"data-toggle":"collapse"})
+
+    for person in a:
+        divs = person.find_all("div")
+        newStudent = Student(id=divs[1].text, name=divs[0].text)
+        studentsList.append(newStudent)
+
+
+def getLetterIndex(i):
+    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    return letters[i - 1]
